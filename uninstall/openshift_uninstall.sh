e9cbbdd25a90c573257bc23f3501f5dfc9bee46b
Christian Hernandez <christianh814@users.noreply.github.com>
2016-09-30 02:11:14 -0400

    Update openshift_uninstall.sh
    Made the script endeavor to return "all green" in ansible

--- uninstall/openshift_uninstall.sh
+++ uninstall/openshift_uninstall.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+show_help () {
+cat <<HELP
+${0} [ --etcd | --help ]
+    --etcd - Use this if you have etcd nodes
+    --help - prints this screen
+HELP
+exit
+}
+[[ $1 = "--help" ]] && show_help
 [[ $1 = "--etcd" ]] && etcd=true
 
-ansible all -m shell -a 'yum remove -y openshift* atomic* openvswitch'
+ansible all -m shell -a 'yum remove -y openshift* atomic* openvswitch || true'
  
 ansible all -m shell -a 'rm -rf /etc/origin/* || true '
 ansible all -m shell -a 'rm -rf /etc/sysconfig/atomic* || true '
@@ -11,7 +20,7 @@ ansible all -m shell -a 'rm -rf /var/lib/origin || true'
  
 if [ ${etcd:=false} = "true" ]; then
   echo "yes"
-  ansible etcd -m shell -a 'yum remove -y etcd'
+  ansible etcd -m shell -a 'yum remove -y etcd || true'
   ansible etcd -m shell -a 'rm -rf /var/lib/etcd || true'
   ansible etcd -m shell -a 'rm -rf /etc/etcd || true'
 fi
